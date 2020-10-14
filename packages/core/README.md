e2be809ddb740f1e1de552aa0915a3acce925f2a
Shamin <shamin616@gmail.com>
2020-10-14 10:22:58 +0530

    Adding colors to core

---
+++ packages/core/README.md
@@ -0,0 +1,2 @@
+# @space-kit/core
+Core of spacekit
---
+++ packages/core/src/colors.ts
@@ -0,0 +1,63 @@
+interface ColorsType {
+  [key: string]: string;
+}
+
+const colors: ColorsType = {
+  white: '#FFFFFF',
+  neutral1: '#F9FAFB',
+  neutral2: '#F4F5F7',
+  neutral3: '#E4E7EB',
+  neutral4: '#D2D6DB',
+  neutral5: '#A0A6B1',
+  neutral6: '#6C7280',
+  neutral7: '#374151',
+  neutral8: '#252F3F',
+  neutral9: '#161D2E',
+  black: '#000000',
+
+  red50: '#FFEBEE',
+  red100: '#FFCDD2',
+  red200: '#EF9A9A',
+  red300: '#E57373',
+  red400: '#EF5350',
+  red500: '#F44336',
+  red600: '#E53935',
+  red700: '#D32F2F',
+  red800: '#C62828',
+  red900: '#B71C1C',
+
+  blue50: '#E3F2FD',
+  blue100: '#BBDEFB',
+  blue200: '#90CAF9',
+  blue300: '#64B5F6',
+  blue400: '#42A5F5',
+  blue500: '#2196F3',
+  blue600: '#1E88E5',
+  blue700: '#1976D2',
+  blue800: '#1565C0',
+  blue900: '#0D47A1',
+
+  green50: '#E8F5E9',
+  green100: '#C8E6C9',
+  green200: '#A5D6A7',
+  green300: '#81C784',
+  green400: '#66BB6A',
+  green500: '#4CAF50',
+  green600: '#43A047',
+  green700: '#388E3C',
+  green800: '#2E7D32',
+  green900: '#1B5E20',
+
+  yellow50: '#FFFDE7',
+  yellow100: '#FFF9C4',
+  yellow200: '#FFF59D',
+  yellow300: '#FFF176',
+  yellow400: '#FFEE58',
+  yellow500: '#FFEB3B',
+  yellow600: '#FDD835',
+  yellow700: '#FBC02D',
+  yellow800: '#F9A825',
+  yellow900: '#F57F17',
+};
+
+export default colors;
