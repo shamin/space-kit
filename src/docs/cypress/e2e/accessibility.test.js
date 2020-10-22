3b0d1f0e494a736f755c330afa2935735c293cac
Ashik Meerankutty <ashik9591@gmail.com>
2020-10-22 15:41:30 +0530

    Refractor pages

--- src/docs/cypress/e2e/accessibility.test.js
+++ src/docs/cypress/e2e/accessibility.test.js
@@ -1,25 +1,19 @@
 /// <reference types="Cypress" />
 
+const pages = ['Button', 'Portal', 'Themes'];
+
 describe('Accessibility tests', () => {
   beforeEach(() => {
     cy.visit('/').get('main').injectAxe();
   });
+
   it('Has no detectable accessibility violations on load', () => {
     cy.checkA11y();
   });
-  it('Navigates to button page and checks for accessibility violations', () => {
-    cy.findByText(/Button/i)
-      .click()
-      .checkA11y();
-  });
-  it('Navigates to portal page and checks for accessibility violations', () => {
-    cy.findByText(/Portal/i)
-      .click()
-      .checkA11y();
-  });
-  it('Navigates to themes page and checks for accessibility violations', () => {
-    cy.findByText(/Themes/i)
-      .click()
-      .checkA11y();
+
+  pages.forEach((page) => {
+    it(`Navigates to ${page} page and checks for accessibility violations`, () => {
+      cy.findByText(page).click().checkA11y();
+    });
   });
 });
