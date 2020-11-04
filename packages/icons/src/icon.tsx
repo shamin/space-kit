2dd882e800c333921f15bc91ee7abf557a61bbca
Shamin Meerankutty <8272719+shamin@users.noreply.github.com>
2020-11-04 12:14:29 +0530

    Color props to icons component, other fixes (#20)
    * Fixes in icon component
    
    * Making size and colors optional

--- packages/icons/src/icon.tsx
+++ packages/icons/src/icon.tsx
@@ -1,20 +1,22 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { colors } from '@space-kit/core';
 
 interface IconProps {
   paths: string[];
   name: string;
-  size: number;
+  size: 16 | 20;
+  color: string;
 }
 
-export const Icon: React.FC<IconProps> = ({ paths, name, size }: IconProps) => {
+export const Icon: React.FC<IconProps> = ({
+  paths,
+  name,
+  size,
+  color = colors.black,
+}: IconProps) => {
   return (
-    <svg
-      style={{ fill: 'rebeccapurple' }}
-      width={size}
-      height={size}
-      data-icon={name}
-    >
+    <svg style={{ fill: color }} width={size} height={size} data-icon={name}>
       {paths.map((d, i) => (
         <path key={i} d={d} fillRule="evenodd" />
       ))}
